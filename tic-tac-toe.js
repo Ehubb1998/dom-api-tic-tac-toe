333bb5d118fd954c0885d9202e0e557558ae1f41
Ranson Knorr <ransonknorr@gmail.com>
2020-08-06 17:00:57 -0500

    phase 1

---
+++ tic-tac-toe.js
@@ -0,0 +1,26 @@
+const square = document.getElementById('tic-tac-toe-board');
+const currentPlayerSymbol = 'x';
+const squareValues = ['', '', '', '', '', '', '', '', '']
+
+
+// let count = 0;
+window.addEventListener('DOMContentLoaded', event => {
+
+    square.addEventListener('click', event => {
+        if (event.target.id.startsWith('square-'){
+
+        })
+})
+    // if (!event.target.classList.contains('x' || 'o')) {
+    //     count++;
+    //     console.log(count);
+    //     if (count % 2 !== 0) {
+    //         event.target.classList.add('x')
+    //     } else {
+    //         event.target.classList.add('o')
+
+    //     }
+    // }
+
+
+})

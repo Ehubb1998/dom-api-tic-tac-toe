cdbb84d5871d311f4b7bcad91b6b959ca792bb45
Elijah <ehubb15@gmail.com>
2020-08-07 18:56:07 -0400

    Finished phase 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -28,5 +28,88 @@ window.addEventListener("DOMContentLoaded", event => {
         } else {
             currentPlayer = "x";
         }
+
+        gameRow();
+        gameColumn();
+        gameDiagonal();
+        tie();
+        reloadButton();
     })
-})
+
+    let whoWon = "";
+    function gameRow() {
+        let winner = document.getElementById("game-status");
+        if (gameArr[0] !== "" && gameArr[0] === gameArr[1] && gameArr[0] === gameArr[2] && gameArr[1] === gameArr[2]) {
+            whoWon = gameArr[0];
+            winner.innerHTML = whoWon.toUpperCase() + " Wins";
+        }
+
+        if (gameArr[3] !== "" && gameArr[3] === gameArr[4] && gameArr[3] === gameArr[5] && gameArr[4] === gameArr[5]) {
+            whoWon = gameArr[3];
+            winner.innerHTML = whoWon.toUpperCase() + " Wins";
+        }
+
+        if (gameArr[6] !== "" && gameArr[6] === gameArr[7] && gameArr[6] === gameArr[8] && gameArr[7] === gameArr[8]) {
+            whoWon = gameArr[6];
+            winner.innerHTML = whoWon.toUpperCase() + " Wins";
+        }
+    }
+
+    function gameColumn() {
+        let winner = document.getElementById("game-status");
+        if (gameArr[0] !== "" && gameArr[0] === gameArr[3] && gameArr[0] === gameArr[6] && gameArr[3] === gameArr[6]) {
+            whoWon = gameArr[0];
+            winner.innerHTML = whoWon.toUpperCase() + " Wins";
+        }
+
+        if (gameArr[1] !== "" && gameArr[1] === gameArr[4] && gameArr[1] === gameArr[7] && gameArr[4] === gameArr[7]) {
+            whoWon = gameArr[1];
+            winner.innerHTML = whoWon.toUpperCase() + " Wins";
+        }
+
+        if (gameArr[2] !== "" && gameArr[2] === gameArr[5] && gameArr[2] === gameArr[8] && gameArr[5] === gameArr[8]) {
+            whoWon = gameArr[2];
+            winner.innerHTML = whoWon.toUpperCase() + " Wins";
+        }
+    }
+
+    function gameDiagonal() {
+        let winner = document.getElementById("game-status");
+        if (gameArr[0] !== "" && gameArr[0] === gameArr[4] && gameArr[0] === gameArr[8] && gameArr[4] === gameArr[8]) {
+            whoWon = gameArr[0];
+            winner.innerHTML = whoWon.toUpperCase() + " Wins";
+        }
+
+        if (gameArr[2] !== "" && gameArr[2] === gameArr[4] && gameArr[2] === gameArr[6] && gameArr[4] === gameArr[6]) {
+            whoWon = gameArr[2];
+            winner.innerHTML = whoWon.toUpperCase() + " Wins";
+        }
+    }
+
+    let counter = 0;
+    function tie() {
+        counter++;
+        console.log(counter);
+        let winner = document.getElementById("game-status");
+        if (counter === 9 && whoWon === "") {
+            winner.innerHTML = "Y'all Suck..."
+        }
+    }
+
+    function reloadButton() {
+        let winner = document.getElementById("game-status");
+        let reload = document.getElementById("new-game");
+        if (whoWon !== "" || winner.innerHTML === "Y'all Suck...") {
+            reload.addEventListener("click", event => {
+                location.reload();
+            })
+        } else {
+            giveup();
+        }
+    }
+
+    function giveUp() {
+        
+    }
+
+});
